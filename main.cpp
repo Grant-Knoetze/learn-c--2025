473dce3baca110eb1981f347b44154094d981efd
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2025-01-14 22:36:14 +0200

    Save file in new folder.
    Save file in new folder for easier commit.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+// Modern C++ Variable Initialization
+// Global variables are initialized outside the main function
+double rate{ 28.5 };
+
+int main()
+{
+	int xyz{10}; // Local variables are initialized inside the main function
+}
